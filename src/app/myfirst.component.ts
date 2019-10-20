c4dc6bb359608e61d8057b298e240fa59a0c7937
Dagmar Cirino <dagmar.mv@gmail.com>
2019-10-20 18:29:04 -0300

    commitando depois de 1 mês

---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '.myfirst',
+  templateUrl: './myfirst.component.html',
+  styleUrls: ['./myfirst.component.css']
+})
+
+export class MyFirstComponent {
+
+}
